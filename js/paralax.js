0d5327b05a3188fe03751ea02d59b1a9a9610955
Egor <egor.shelemetev228@mail.ru>
2024-09-06 20:26:08 +1000

    first commit

--- js/paralax.js
+++ js/paralax.js
@@ -3,7 +3,7 @@ window.addEventListener("scroll", (e) => {
     let y = document.querySelector('.delivery').getBoundingClientRect().y
 
     if (y < 621 && y > 100) {
-        document.querySelector('.delivery--img').style.transform = `translate(-50%,${y/2}px)`
+        document.querySelector('.delivery--img').style.transform = `translate(-50%,${(y/2) - 100}px)`
     }
 })
 
@@ -13,8 +13,9 @@ window.addEventListener("scroll", (e) => {
     let y = document.querySelector('.about').getBoundingClientRect().y
     // document.querySelector('.about__back--1').style.transform = `translate(-50%,${y/2}px)`
 
-    if (y < 721 && y > -100) {
-        document.querySelector('.about__back--1').style.transform = `translate(-50%,-${(y / 2) + 50}px)`
-        document.querySelector('.about__back--2').style.transform = `translate(-50%,${(y / 2) + 50}px)`
+    if (y < 621 && y > 100) {
+        document.querySelector('.about__back--1').style.transform = `translate(-50%,-${(y / 2) - 100}px)`
+        document.querySelector('.about__back--2').style.transform = `translate(-50%,${(y / 2) - 90}px)`
     }
 })
+
